7cbaa5746e4f58bb2e8749922eb8ff95d9437d95
Hùng Lê <hungvanle321@gmail.com>
2021-12-06 20:43:34 +0700

    File source code

--- LWZ/Source.cpp
+++ LWZ/Source.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
-//#include <map>
-#include <vector>
 #include <unordered_map>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#define BITS 12  
 using namespace std;
-
-//source https://www.geeksforgeeks.org/lzw-lempel-ziv-welch-compression-technique/
-
 vector<int> encoding(string s1)
 {
     cout << "Encoding\n";
@@ -41,7 +40,38 @@ vector<int> encoding(string s1)
     output_code.push_back(table[p]);
     return output_code;
 }
+//Viet so nguyen ra file output
+void output_code1(ofstream& output, unsigned int code)
+{
+    static int output_bit_count = 0;
+    static unsigned long output_bit_buffer = 0L;
+    output_bit_buffer |= (unsigned long)code << (32 - BITS - output_bit_count);
+    output_bit_count += BITS;
+    while (output_bit_count >= 8)
+    {
+        output.put((char)(output_bit_buffer >> 24));
+        output_bit_buffer <<= 8;
+        output_bit_count -= 8;
+    }
+}
+//Nhap so nguyen tu file input
+int input_code(ifstream& input)
+{
+    unsigned int return_value;
+    static int input_bit_count = 0;
+    static unsigned long input_bit_buffer = 0L;
 
+    while (input_bit_count <= 24)
+    {
+        input_bit_buffer |=
+            (unsigned long)input.get() << (24 - input_bit_count);
+        input_bit_count += 8;
+    }
+    return_value = input_bit_buffer >> (32 - BITS);
+    input_bit_buffer <<= BITS;
+    input_bit_count -= BITS;
+    return(return_value);
+}
 void decoding(vector<int> op)
 {
     cout << "\nDecoding\n";
@@ -74,15 +104,47 @@ void decoding(vector<int> op)
         old = n;
     }
 }
+int size(const char* filename) {
+    ifstream f(filename, ios::binary);
+    f.seekg(0, ifstream::end);
+    int size1 = f.tellg();
+    f.seekg(0);
+    f.close();
+    return size1;
+}
 int main()
 {
 
     string s = "WYS*WYGWYS*WYSWYSG";
     vector<int> output_code = encoding(s);
-    cout << "Output Codes are: ";
+
+    ofstream fout("output.lzw", ios::binary);
+    cout << "Output Codes: " << endl;
     for (int i = 0; i < output_code.size(); i++) {
         cout << output_code[i] << " ";
+        output_code1(fout, output_code[i]);
     }
+
+    output_code1(fout, 1);
+    output_code1(fout, 0);
+    fout.close();
+
+
+
+    vector<int> input_code1;
+    ifstream fin("output.lzw", ios::binary);
+    int n = -1;
+    while (n != 1 && !fin.eof()) {
+        n = input_code(fin);
+        input_code1.push_back(n);
+    }
+    input_code1.pop_back();
     cout << endl;
-    decoding(output_code);
+    cout << "Input Codes: " << endl;
+    for (int i = 0; i < input_code1.size(); i++)
+        cout << input_code1[i] << " ";
+    decoding(input_code1);
+    fin.close();
+    cout << endl << "Kich thuoc file goc: " << 8 * s.length() << " bits" << endl;
+    cout << "Kich thuoc file .lzw: " << 8 * size("output.lzw") << " bits" << endl;
 }
