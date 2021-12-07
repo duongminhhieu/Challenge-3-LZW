61630008868243dd1c4909e5f91d959e46e587ff
Bùi Thanh Tùng <tung.bt.dongthanh@gmail.com>
2021-12-07 12:04:23 +0700

    them tham so dong lenh

--- LWZ/Source.cpp
+++ LWZ/Source.cpp
@@ -5,9 +5,9 @@
 #include <fstream>
 #define BITS 12  
 using namespace std;
-vector<int> encoding(string s1)
+// order=1 in từ điển
+vector<int> encoding(string s1,bool order)
 {
-    cout << "Encoding\n";
     unordered_map<string, int> table;
     for (int i = 0; i <= 255; i++) {
         string ch = "";
@@ -19,7 +19,7 @@ vector<int> encoding(string s1)
     p += s1[0];
     int code = 256;
     vector<int> output_code;
-    cout << "String\tOutput_Code\tAddition\n";
+ //   cout << "String\tOutput_Code\tAddition\n";
     for (int i = 0; i < s1.length(); i++) {
         if (i != s1.length() - 1)
             c += s1[i + 1];
@@ -27,8 +27,12 @@ vector<int> encoding(string s1)
             p = p + c;
         }
         else {
-            cout << p << "\t" << table[p] << "\t\t"
-                << p + c << "\t" << code << endl;
+//            cout << p << "\t" << table[p] << "\t\t"
+ //               << p + c << "\t" << code << endl;
+            if (order==1){
+            cout<<p+c<<"\t"<<code<<endl;
+                }
+
             output_code.push_back(table[p]);
             table[p + c] = code;
             code++;
@@ -36,7 +40,7 @@ vector<int> encoding(string s1)
         }
         c = "";
     }
-    cout << p << "\t" << table[p] << endl;
+//    cout << p << "\t" << table[p] << endl;
     output_code.push_back(table[p]);
     return output_code;
 }
@@ -72,9 +76,10 @@ int input_code(ifstream& input)
     input_bit_count -= BITS;
     return(return_value);
 }
-void decoding(vector<int> op)
+string decoding(vector<int> op,bool order)
 {
-    cout << "\nDecoding\n";
+    string str = "";
+
     unordered_map<int, string> table;
     for (int i = 0; i <= 255; i++) {
         string ch = "";
@@ -85,7 +90,8 @@ void decoding(vector<int> op)
     string s = table[old];
     string c = "";
     c += s[0];
-    cout << s;
+  //  cout << s;
+    str += s;
     int count = 256;
     for (int i = 0; i < op.size() - 1; i++) {
         n = op[i + 1];
@@ -96,13 +102,21 @@ void decoding(vector<int> op)
         else {
             s = table[n];
         }
-        cout << s;
+   //     cout << s;
+        str += s;
+
         c = "";
         c += s[0];
         table[count] = table[old] + c;
+
+        if (order == 1) {
+            cout << table[count] << ": "<<count << endl;
+        }
         count++;
         old = n;
     }
+
+    return str;
 }
 int size(const char* filename) {
     ifstream f(filename, ios::binary);
@@ -112,9 +126,158 @@ int size(const char* filename) {
     f.close();
     return size1;
 }
-int main()
-{
 
+string input_txt(string file){
+    ifstream f;
+    f.open(file);
+    
+    string s="";
+    if (f.is_open()){
+        string line;
+        while (getline(f, line)) {
+            if (s != "") {
+                s += '\n' + line;
+            }
+            else {
+                s += line;
+            }
+        }
+    }
+    f.close();
+    return s;
+}
+
+void output_txt(string file,string s){
+    ofstream f;
+    f.open(file);
+
+    if(f.is_open()){
+        f<<s;
+    }
+    f.close();
+
+}
+
+int main(int argc,char *argv[])
+{
+    if (argc != 5) {
+		cout << "Input is invalid!";
+		return 0;
+	}
+
+    string action=argv[1];
+    string inputPath=argv[2];
+    string outputPath=argv[3];
+    string outputInfo=argv[4];
+
+    //string action = "-d";
+    //string inputPath = "Output.lwz";
+    //string outputPath = "Output.txt";
+    //string outputInfo = "-d";
+
+
+    if (action=="-e"){
+        string s=input_txt(inputPath);
+        cout << "Encoding\n";
+        cout << "Input Text: " << s << endl;
+
+        vector<int> output_code;
+
+        if (outputInfo=="-i"){
+
+            output_code=encoding(s,0);
+
+
+            ofstream fout(outputPath, ios::binary);
+            cout << "Output Codes: " << endl;
+            for (int i = 0; i < output_code.size(); i++) {
+            cout << output_code[i] << " ";
+            output_code1(fout, output_code[i]);
+
+            }
+
+            output_code1(fout, 1);
+            output_code1(fout, 0);
+            fout.close();
+
+            cout << "Input Size: " << 8 * s.length() << " bits" << endl;
+            cout << "Output Size: " << 8 * size(outputPath) << " bits" << endl;
+            cout<<"Space saved: "<<(1-1.0*size(outputPath)/s.length())*100<<" %"<<endl;
+        }
+        else{
+            cout << "----Dictionary----" << endl;
+            output_code=encoding(s,1);
+            cout << "------------------" << endl;
+
+
+            ofstream fout(outputPath, ios::binary);
+            cout << "Output Codes: " << endl;
+            for (int i = 0; i < output_code.size(); i++) {
+            cout << output_code[i] << " ";
+            output_code1(fout, output_code[i]);
+
+            }
+            output_code1(fout, 1);
+            output_code1(fout, 0);
+            fout.close();
+
+        }
+
+        if (outputInfo=="-ind"){
+            cout << "\nInput Size: " << 8 * s.length() << " bits" << endl;
+            cout << "Output Size: " << 8 * size(outputPath) << " bits" << endl;
+            cout<<"Space saved: "<<(1-1.0*size(outputPath)/s.length())*100<<" %"<<endl;
+        }
+
+    }
+    else{
+
+        vector<int> input_code1;
+        ifstream fin(inputPath, ios::binary);
+        int n = -1;
+        while (n != 1 && !fin.eof()) {
+            n = input_code(fin);
+            input_code1.push_back(n);
+        }
+        input_code1.pop_back();
+        fin.close();
+
+        cout << "Decoding\n";
+
+        cout << "Input Codes: " << endl;
+        for (int i = 0; i < input_code1.size(); i++)
+            cout << input_code1[i] << " ";
+        cout << endl;
+
+        string s;
+        if (outputInfo == "-i") {
+
+            s = decoding(input_code1, 0);
+            output_txt(outputPath, s);
+
+            cout << "\nOutput Text: " <<s<<endl;
+            cout << "Input Size: " << 8 * size(inputPath) << " bits" << endl;
+            cout << "Output Size: " << 8 * s.length() << " bits" << endl;
+//            cout << "Space saved: " << (1 - 1.0 * size(outputPath) / s.length()) * 100 << " %" << endl;
+        }
+        else {
+            // xuat tu dien
+            cout << "----Dictionary----" << endl;
+            s = decoding(input_code1, 1);
+            cout << "------------------" << endl;
+            output_txt(outputPath, s);
+            cout << "Output Text: " << s << endl;
+        }
+
+        if (outputInfo == "-ind") {
+
+            cout << "Input Size: " << 8 * size(inputPath) << " bits" << endl;
+            cout << "Output Size: " << 8 * s.length() << " bits" << endl;
+        }
+
+    }
+    
+/*
     string s = "WYS*WYGWYS*WYSWYSG";
     vector<int> output_code = encoding(s);
 
@@ -128,23 +291,6 @@ int main()
     output_code1(fout, 1);
     output_code1(fout, 0);
     fout.close();
+*/
 
-
-
-    vector<int> input_code1;
-    ifstream fin("output.lzw", ios::binary);
-    int n = -1;
-    while (n != 1 && !fin.eof()) {
-        n = input_code(fin);
-        input_code1.push_back(n);
-    }
-    input_code1.pop_back();
-    cout << endl;
-    cout << "Input Codes: " << endl;
-    for (int i = 0; i < input_code1.size(); i++)
-        cout << input_code1[i] << " ";
-    decoding(input_code1);
-    fin.close();
-    cout << endl << "Kich thuoc file goc: " << 8 * s.length() << " bits" << endl;
-    cout << "Kich thuoc file .lzw: " << 8 * size("output.lzw") << " bits" << endl;
 }
